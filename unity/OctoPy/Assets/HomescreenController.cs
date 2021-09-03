e48731ce5d8d6faad63a1d2fabc0ede2369e9bf9
kaizerxcx <johnkaizer.tabanag@cit.edu>
2021-09-03 18:05:21 +0800

    WordKit Compability Fix

--- unity/OctoPy/Assets/HomescreenController.cs
+++ unity/OctoPy/Assets/HomescreenController.cs
@@ -3,22 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class HomescreenController : MonoBehaviour
 {
     // Start is called before the first frame update
     public Button crazeOnPhonics;
     public Button wordKit;
+    public TextMeshProUGUI fronttext;
+    public GameObject WordKitLock;
+    private int i = 0;
     void Start()
     {
        /* gameObject.AddComponent<UnityMessageManager>();
         UnityMessageManager.Instance.SendMessageToFlutter("HomeScreen");*/
         crazeOnPhonics.onClick.AddListener(startCraze);
        wordKit.onClick.AddListener(startWord);
+          
+      
     }
     private void FixedUpdate()
     {
         SoundManagerScript.playSound("Homescreen");
+        StartCoroutine(ExecuteAfterTime(0.1f));
+        fronttext.SetText(" " +i);
+        if (i > 27)
+        {
+            WordKitLock.SetActive(true);
+            wordKit.interactable = false;
+        }
     }
     // Update is called once per frame
     void Update()
@@ -42,4 +55,20 @@ public class HomescreenController : MonoBehaviour
     {
         SceneManager.LoadScene("WordKit");
     }
+    /*  public int GetSDKLevel()
+      {
+          var clazz = AndroidJNI.FindClass("android.os.Build$VERSION");
+          var fieldID = AndroidJNI.GetStaticFieldID(clazz, "SDK_INT", "I");
+          var sdkLevel = AndroidJNI.GetStaticIntField(clazz, fieldID);
+          return sdkLevel;
+      }*/
+    IEnumerator ExecuteAfterTime(float time)
+    {
+        var clazz = AndroidJNI.FindClass("android/os/Build$VERSION");
+        var fieldID = AndroidJNI.GetStaticFieldID(clazz, "SDK_INT", "I");
+        var sdkLevel = AndroidJNI.GetStaticIntField(clazz, fieldID);
+        i = sdkLevel;
+        yield return new WaitForSeconds(time);
+    }
+
 }
